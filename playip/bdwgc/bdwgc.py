eaee1d7067a8910fd3b9df761e6b07243ac773da
jesjf <jorge.filho@jambeiro.com.br>
2021-10-12 23:01:33 -0300

    Ajustes

--- playip/bdwgc/bdwgc.py
+++ playip/bdwgc/bdwgc.py
@@ -11,10 +11,10 @@ wgcrouter = APIRouter(prefix="/playipispbd/basic")
 gwbd = None
 
 def getWDB():
-    global gwdb
-    if not gwdb:
-        gwdb = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+settings.server+';DATABASE='+settings.database+';UID='+settings.username+';PWD='+ settings.password)
-    return gwdb
+    global gwbd
+    if not gwbd:
+        gwbd = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+settings.server+';DATABASE='+settings.database+';UID='+settings.username+';PWD='+ settings.password)
+    return gwbd
 
 class ContractData(pydantic.BaseModel):
     id_contract: str
