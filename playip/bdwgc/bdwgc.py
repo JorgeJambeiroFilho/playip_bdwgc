412bd95dbc446c0b7255ae2c1a2c633ec9e00581
jesjf <jorge.filho@jambeiro.com.br>
2021-10-16 08:27:20 -0300

    Incluí informação de media (radio/cabo).

--- playip/bdwgc/bdwgc.py
+++ playip/bdwgc/bdwgc.py
@@ -20,34 +20,63 @@ class ContractData(pydantic.BaseModel):
     id_contract: str
     download_speed: int
     upload_speed:int
+    is_radio:bool
     found:bool
     pack_name:str
 
 @wgcrouter.get("/getcontract/{id_contract}", response_model=ContractData)
 async def getContract(id_contract:str) -> ContractData:
     wdb = getWDB()
+
+    #existe uma tabela chamada Servico que tinha tudo para ser relevante aqui, mas não foi
     with wdb.cursor() as cursor:
         cursor.execute("""
-          SELECT  
-                NM_PACOTE_SERVICO,VL_DOWNLOAD, VL_UPLOAD, s.VL_SERVICO, DT_ATIVACAO, DT_DESATIVACAO
+        
+          SELECT  NM_PACOTE_SERVICO, VL_DOWNLOAD, VL_UPLOAD, s.VL_SERVICO, DT_ATIVACAO, DT_DESATIVACAO, 
+                  dici.ID_TIPO_MEIO_ACESSO, dici.ID_TIPO_TECNOLOGIA, dici.ID_TIPO_PRODUTO, 
+                  tmeio.TX_DESCRICAO_TIPO, ttec.TX_DESCRICAO_TIPO, tprod.TX_DESCRICAO_TIPO
           FROM 
-                ContratoItem as ci, PacoteServico as ps, PacoteServico_Servico as s, Contrato_PacoteServico_Servico as cps
+                  ContratoItem as ci, PacoteServico as ps, PacoteServico_Servico as s, Contrato_PacoteServico_Servico as cps, Servico_DICI as dici,
+                  TiposDiversos as tmeio, TiposDiversos as ttec, TiposDiversos as tprod
           WHERE 
                 ci.ID_CONTRATO={param_id_contrato} and 
                 ci.ID_PACOTE_SERVICO=ps.ID_PACOTE_SERVICO and 
                 ps.ID_PACOTE_SERVICO=s.ID_PACOTE_SERVICO and 
                 cps.ID_SERVICO=s.ID_SERVICO and
                 cps.ID_CONTRATO=ci.ID_CONTRATO and
+                dici.ID_SERVICO=cps.ID_SERVICO and
+                tmeio.ID_TIPO_DIVERSOS=dici.ID_TIPO_MEIO_ACESSO and
+                ttec.ID_TIPO_DIVERSOS=dici.ID_TIPO_TECNOLOGIA and
+                tprod.ID_TIPO_DIVERSOS=dici.ID_TIPO_PRODUTO and
                 s.VL_DOWNLOAD > 0
           ORDER BY
-                    DT_ATIVACAO DESC          """.format(param_id_contrato=id_contract))
+                    DT_ATIVACAO DESC
+                
+                         """.format(param_id_contrato=id_contract))
         row = cursor.fetchone()
-        if not row:
+        if not row or row[5]: #se tem data de desativação, não vale
             res = ContractData(id_contract=id_contract, found=False)
         else:
             name = row[0]
             dl = row[1]
             ul =row[2]
-            res = ContractData(id_contract=id_contract,download_speed=dl,upload_speed=ul,pack_name=name,found=True)
+            is_radio = row[9]=="radio" # a outra opção no wgc é "fibra"
+
+            res = ContractData(id_contract=id_contract, download_speed=dl, upload_speed=ul, pack_name=name, is_radio=is_radio, found=True)
 
     return res
+
+
+          # SELECT
+          #       NM_PACOTE_SERVICO,VL_DOWNLOAD, VL_UPLOAD, s.VL_SERVICO, DT_ATIVACAO, DT_DESATIVACAO
+          # FROM
+          #       ContratoItem as ci, PacoteServico as ps, PacoteServico_Servico as s, Contrato_PacoteServico_Servico as cps
+          # WHERE
+          #       ci.ID_CONTRATO={param_id_contrato} and
+          #       ci.ID_PACOTE_SERVICO=ps.ID_PACOTE_SERVICO and
+          #       ps.ID_PACOTE_SERVICO=s.ID_PACOTE_SERVICO and
+          #       cps.ID_SERVICO=s.ID_SERVICO and
+          #       cps.ID_CONTRATO=ci.ID_CONTRATO and
+          #       s.VL_DOWNLOAD > 0
+          # ORDER BY
+          #           DT_ATIVACAO DESC
