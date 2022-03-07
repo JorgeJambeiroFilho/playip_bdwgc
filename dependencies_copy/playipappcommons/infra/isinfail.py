56413a27416af7ceb3cc0319c8430bceacf4a5fe
jesjf <jorge.filho@jambeiro.com.br>
2022-03-07 17:49:06 -0300

    Ajustes.

---
+++ dependencies_copy/playipappcommons/infra/isinfail.py
@@ -0,0 +1,23 @@
+from playipappcommons.infra.endereco import Endereco
+from playipappcommons.infra.infraimportmethods import findAddress
+from playipappcommons.infra.inframethods import isAddressInFailIntern, getInfraElementFailState
+from playipappcommons.infra.inframodels import AddressInFail, InfraElement, AddressQuery
+from playipappcommons.playipchatmongo import getBotMongoDB
+
+
+async def isAddressInFail(addressQuery: AddressQuery) -> AddressInFail:
+    if not addressQuery.endereco:
+        return await isAddressInFailIntern(addressQuery)
+    else:
+        return await isInFail(addressQuery.endereco)
+
+async def isInFail(endereco: Endereco) -> AddressInFail:
+    mdb = getBotMongoDB()
+    infraElement: InfraElement = await findAddress(mdb, endereco)
+    if infraElement is None:
+        return AddressInFail(located=False, inFail= False)
+    else:
+        res: AddressInFail = await getInfraElementFailState(infraElement.id)
+        return res
+
+
