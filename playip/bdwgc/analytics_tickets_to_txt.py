db7bdcc777057241169a48baca697665ca672725
jesjf <jorge.filho@jambeiro.com.br>
2022-09-05 18:34:14 -0300

    Fiz a exportação do BD parea txt usar settings.

--- playip/bdwgc/analytics_tickets_to_txt.py
+++ playip/bdwgc/analytics_tickets_to_txt.py
@@ -1,13 +1,20 @@
 from playip.bdwgc.sql_import_analytics_tickets import sql_analytics_tickets
+from dynaconf import settings
 
 import pyodbc
 import datetime
 
 
-server = '170.238.84.12'
-database = 'WGC'
-username = 'wgcplayip'
-password = 'teste01*'
+# server = '170.238.84.12'
+# database = 'WGC'
+# username = 'wgcplayip'
+# password = 'teste01*'
+
+server = settings.SERVER
+database = settings.DATABASE
+username = settings.USERNAME
+password = settings.PASSWORD
+
 cnxn = pyodbc.connect(
     'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
 cursor = cnxn.cursor()
