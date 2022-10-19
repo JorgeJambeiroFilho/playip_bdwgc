53a7a0542a3f7fa377c45d3e0c48ff7555edbb9c
jesjf <jorge.filho@jambeiro.com.br>
2022-10-19 20:04:35 -0300

    Mesmo ajuste na importação para o MongoDB

--- playip/bdwgc/import_addr.py
+++ playip/bdwgc/import_addr.py
@@ -69,9 +69,9 @@ async def importAddressesIntern(mdb, iar:ImportAddressResult):
                     LEFT JOIN Condominio as Condominio on (Endereco.ID_CONDOMINIO=Condominio.ID_CONDOMINIO)
                     LEFT JOIN LOG_UF as UF on (Cidade.ID_UF_LOCALIDADE=UF.ID_UF)
             GROUP BY
-                    UF.ID_UF, Cidade.ID_LOCALIDADE, Endereco.TX_BAIRRO, Endereco.TX_ENDERECO, Endereco.NR_NUMERO, Endereco.TX_COMPLEMENTO, tmeio.TX_DESCRICAO_TIPO, Endereco.TX_CEP as cep, Condominio.NM_CONDOMINIO as condominio
+                    UF.ID_UF, Cidade.ID_LOCALIDADE, Endereco.TX_BAIRRO, Endereco.TX_ENDERECO, Endereco.NR_NUMERO, Endereco.TX_COMPLEMENTO, tmeio.TX_DESCRICAO_TIPO, Endereco.TX_CEP, Condominio.NM_CONDOMINIO
             ORDER BY 
-                    UF.ID_UF, Cidade.ID_LOCALIDADE, Endereco.TX_BAIRRO, Endereco.TX_ENDERECO, Endereco.NR_NUMERO, Endereco.TX_COMPLEMENTO, tmeio.TX_DESCRICAO_TIPO, Endereco.TX_CEP as cep, Condominio.NM_CONDOMINIO as condominio
+                    UF.ID_UF, Cidade.ID_LOCALIDADE, Endereco.TX_BAIRRO, Endereco.TX_ENDERECO, Endereco.NR_NUMERO, Endereco.TX_COMPLEMENTO, tmeio.TX_DESCRICAO_TIPO, Endereco.TX_CEP, Condominio.NM_CONDOMINIO
                          """)
 
         row = cursor.fetchone()
