8b6607de47463a70ede433dc35c2037231a18251
jesjf <jorge.filho@jambeiro.com.br>
2023-01-08 23:30:41 -0300

    Ajustes.

--- playip/bdwgc/import_addr.py
+++ playip/bdwgc/import_addr.py
@@ -92,7 +92,7 @@ async def importAddressesIntern(mdb, iar:ImportAddressResult):
 
     time_end = time.time()
     iar.done()
-    await iar.saveHardly()
+    await iar.saveHardly(mdb)
     print("Tempo de importação ", time_end - time_ini)
     print(iar)
 
